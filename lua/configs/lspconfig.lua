2ccce870f1950262aaf272746434dfb13af54587
daya1222 <dayajoshi1222@gmail.com>
2025-05-02 13:55:06 +0545

    initial commit

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,23 @@
+local servers = {
+  html = {},
+  awk_ls = {},
+  bashls = {},
+
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "basic",
+          useLibraryCodeForTypes = true,
+          diagnosticMode = "workspace",
+        },
+      },
+    },
+  },
+}
+
+for name, opts in pairs(servers) do
+  vim.lsp.enable(name)  -- nvim v0.11.0 or above required
+  vim.lsp.config(name, opts) -- nvim v0.11.0 or above required
+end
