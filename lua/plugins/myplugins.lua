1e3648526e00e9b3a442d40756acb13825d619f8
daya1222 <dayajoshi1222@gmail.com>
2025-05-02 14:23:59 +0545

    hih

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -13,17 +13,6 @@ local plugins = {
     },
     dependencies = { "nvim-lua/plenary.nvim" },
   },
-},
-{
-  "ahmedkhalf/project.nvim",
-  config = function()
-    require("project_nvim").setup {
-      detection_methods = { "lsp", "pattern" },
-      patterns = { ".git", "Makefile", "package.json", "Cargo.toml" },
-    }
-    require("telescope").load_extension("projects")  -- Optional Telescope support
-  end
 }
-
 return plugins
 
